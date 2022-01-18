778ffc65f46e6c3772337092550cdae363856a13
Arvel Gavrilla R <arvel.gavrilla@gmail.com>
2022-01-18 09:39:10 +0700

    add introduction 2

---
+++ C/Introduction/02. Playing With Characters.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    char ch,s[100],sen[100];
+    scanf("%c\n",&ch);
+    scanf("%s\n",&s);
+    gets(sen);
+    printf("%c\n",ch);
+    printf("%s\n",s);
+    printf("%s",sen);
+    return 0;  
+}
